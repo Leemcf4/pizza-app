76ecebd402a6c3d29ddbd98fd01e4ef4ae2cb847
Leemcf4 <leemcf4@hotmail.co.uk>
2021-02-10 22:02:09 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,1 @@
-import firebase from "firebase"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBH4kejtcVzBKEpLltD6ZAriDX8n2ZcATw",
-  authDomain: "pizza-ebe4c.firebaseapp.com",
-  projectId: "pizza-ebe4c",
-  storageBucket: "pizza-ebe4c.appspot.com",
-  messagingSenderId: "286707409077",
-  appId: "1:286707409077:web:d236415a60160cc7ca1efc",
-  measurementId: "G-CDTD6FSFRY",
-}
-
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-const db = firebaseApp.firestore()
-const auth = firebase.auth()
-
-export { auth }
-export default db
